8bd257e8317e385cbc87b4f254fedaf590c36838
Nick Maassen <nicholasmaassen@yahoo.com>
2023-01-28 22:28:12 -0500

    Adds color coded circles to accordions

--- React/ingredient_investigator/src/components/Results.js
+++ React/ingredient_investigator/src/components/Results.js
@@ -1,25 +1,18 @@
 import React from "react";
 import IngredientList from "./IngredientsList";
+import { threatLevel } from "./threatLevel";
 
-export const threatLevel = {
-    1:"Green",
-    2:"LightGreen",
-    3:"Orange",
-    4:"DarkOrange",
-    5:"Red",
-}
-
-const Results = ({ingredients}) => {
+const Results = ({ ingredients }) => {
     return (
         <div>
-            <h1 className="hazardLevel" style={{color: `${threatLevel[ingredients.HazardValue]}`}}>
-            {ingredients.HazardValue}
+            <h1 style={{ color: `${threatLevel[ingredients.HazardValue]}` }}>
+                {ingredients.HazardValue}
             </h1>
-            <br/>
+            <br />
             {ingredients.HazardText}
-          
-            <IngredientList ingredients={ingredients.Ingredients}/>
+
+            <IngredientList ingredients={ingredients.Ingredients} />
         </div>
     );
-}
+};
 export default Results;
---
+++ React/ingredient_investigator/src/components/threatLevel.js
@@ -0,0 +1,7 @@
+export const threatLevel = {
+    1: "Green",
+    2: "LightGreen",
+    3: "Orange",
+    4: "DarkOrange",
+    5: "Red",
+};
